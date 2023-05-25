6e9860c78fbb37bf11bfed9d84042bf99ff07cb5
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-05-25 17:48:31 -0300

    feat: make Feed extend from GlobalFeed

--- src/feed/Feed.java
+++ src/feed/Feed.java
@@ -2,13 +2,11 @@ package feed;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /*Esta clase modela la lista de articulos de un determinado feed*/
-public class Feed {
+public class Feed extends GlobalFeed {
 	String siteName;
-	List<Article> articleList;
-	
+
 	public Feed(String siteName) {
 		super();
 		this.siteName = siteName;
@@ -23,40 +21,17 @@ public class Feed {
 		this.siteName = siteName;
 	}
 	
-	public List<Article> getArticleList(){
-		return articleList;
-	}
-	
-	public void setArticleList(List<Article> articleList){
-		this.articleList = articleList;
-	}
-	
-	public void addArticle(Article a){
-		this.getArticleList().add(a);
-	}
-	
-	public Article getArticle(int i){
-		return this.getArticleList().get(i);
-	}
-	
-	public int getNumberOfArticles(){
-		return this.getArticleList().size();
-	}
-	
 	@Override
 	public String toString(){
 		return "Feed [siteName=" + getSiteName() + ", articleList=" + getArticleList() + "]";
 	}
-	
 
 	public void prettyPrint(){
 		for (Article a: this.getArticleList()){
 			a.prettyPrint();
 		}
-		
 	}
 
-	
 	public static void main(String[] args) {
 		  Article a1 = new Article("This Historically Black University Created Its Own Tech Intern Pipeline",
 			  "A new program at Bowie State connects computing students directly with companies, bypassing an often harsh Silicon Valley vetting process",
