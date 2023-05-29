9db7cc3e160d60d1d282110d15156eae7373333a
Lautaro Rodriguez <lautaro.rodriguez@mi.unc.edu.ar>
2023-05-29 18:35:06 -0300

    refactor: improve readability of flow of execution in main function, as well as avoid code repetition. New features in this commit: - A globalArticleList is used to build a single Feed and avoid several Feed instantiations - New method for extracting the name of sites from their respective urls

--- src/FeedReaderMain.java
+++ src/FeedReaderMain.java
@@ -1,140 +1,141 @@
+import java.util.ArrayList;
 import java.util.List;
-
-import httpRequest.httpRequester;
-
 import feed.Article;
 import feed.Feed;
-
+import httpRequest.httpRequester;
 import parser.GeneralParser;
 import parser.RedditParser;
 import parser.RssParser;
 import parser.SubscriptionParser;
-
 import subscription.SingleSubscription;
 import subscription.Subscription;
 
-import namedEntity.heuristic.Heuristic;
-import namedEntity.heuristic.QuickHeuristic;
-
 public class FeedReaderMain {
 
-	private static void printHelp() {
-		System.out.println("Please, call this program in correct way: FeedReader [-ne]");
-	}
+    // articleList global para computar las entidades nombradas y unificar los feeds
+    // en uno solo
+    private static List<Article> globalArticleList = null;
+    // lista de nombres de los sitios de los cuales se obtuvieron los feeds,
+    // se le pasa como parametro al generador de feeds
+    private static List<String> siteNames = new ArrayList<String>();
 
-	public static void main(String[] args) {
+    private static void printHelp() {
+        System.out.println("Please, call this program in correct way: FeedReader [-ne]");
+    }
 
-		if (args.length > 1 || (args.length == 1 && !args[0].equals("-ne"))) {
-			printHelp();
-			return;
-		}
+    private static String extractSiteName(String url) {
+        String siteName = url.substring(url.indexOf("//") + 2, url.indexOf("/", url.indexOf("//") + 2));
 
-		System.out.println("************* FeedReader version 1.0 *************");
-		httpRequester requester = new httpRequester();
+        // extrae la parte de la url que contiene el nombre del sitio
+        // es decir, de www.google.com.ar obtiene google
+        siteName = siteName.substring(siteName.indexOf(".") + 1, siteName.lastIndexOf("."));
 
-		/* Leer el archivo de suscription por defecto */
-		Subscription subscription = new SubscriptionParser()
-				.parse("config/subscriptions.json");
+        if (!siteNames.contains(siteName)) {
+            siteNames.add(siteName);
+        }
+        return siteName;
+    }
 
-		/* Si se llama al programa sin argumentos, se genera el Feed */
-		if (args.length == 0) {
+    public static void main(String[] args) {
 
-			/* Llamar al httpRequester para obtener el feed del servidor */
-			for (int i = 0; i < subscription.getLength(); i++) {
-				SingleSubscription single = subscription.getSingleSubscription(i);
-				String type = single.getUrlType();
-				String rawUrl = single.getUrl();
-				GeneralParser<List<Article>> feedParser = null;
+        if (args.length > 1 || (args.length == 1 && !args[0].equals("-ne"))) {
+            printHelp();
+            return;
+        }
 
-				/*
-				 * llamada al Parser especifico para extrar los datos necesarios por la
-				 * aplicacion
-				 */
-				if (type.equals("rss")) {
-					feedParser = new RssParser();
-				} else if (type.equals("reddit")) {
-					feedParser = new RedditParser();
-				} else {
-					System.out.println("Error: type of feed not supported");
-					continue;
-				}
+        System.out.println("************* FeedReader version 1.0 *************");
+        httpRequester requester = new httpRequester();
 
-				for (int j = 0; j < single.getUlrParamsSize(); j++) {
-					String url = rawUrl.replace("%s", single.getUlrParams(j));
-					String data = requester.getFeed(url, type);
+        // codigo comun a las ejecuciones con y sin parametros
 
-					List<Article> articleList = feedParser.parse(data);
+        // obtengo la lista de suscripciones
+        Subscription subscription = new SubscriptionParser().parse("config/subscriptions.json");
 
-					/* llamada al constructor de Feed */
-					Feed feed = new Feed(url);
-					feed.setArticleList(articleList);
+        // se obtienen las single subscriptions para poder extraer informacion sobre
+        // cada una de ellas
+        List<SingleSubscription> singleSubscriptions = subscription.getSubscriptionsList();
 
-					/*
-					 * llamada al prettyPrint del Feed para ver los articulos del feed en forma
-					 * legible y amigable para el usuario
-					 */
-					feed.prettyPrint();
-				}
-			}
+        // se itera sobre cada una de las single subscriptions y se extrae
+        // single.getUrlType()
+        // single.getUrl()
+        // luego se asigna el parser correspondiente segun el tipo de url
 
-		}
-		/*
-		 * Si se llama al programa con el argumento -ne
-		 * se genera el Feed y se computan las entidades nombradas
-		 */
-		else { // args.length == 1
-			/* Llamar al httpRequester para obtener el feed del servidor */
-			for (int i = 0; i < subscription.getLength(); i++) {
-				SingleSubscription single = subscription.getSingleSubscription(i);
-				String type = single.getUrlType();
-				String rawUrl = single.getUrl();
+        for (int i = 0; i < singleSubscriptions.size(); i++) {
 
-				GeneralParser<List<Article>> feedParser = null;
-				Heuristic heuristic = new QuickHeuristic();
+            // informacion para poder construir el parser
+            SingleSubscription single = singleSubscriptions.get(i);
+            String urlType = single.getUrlType();
+            String url = single.getUrl();
 
-				/*
-				 * llamada al Parser especifico para extrar los datos necesarios por la
-				 * aplicacion
-				 */
-				if (type.equals("rss")) {
-					feedParser = new RssParser();
-				} else if (type.equals("reddit")) {
-					feedParser = new RedditParser();
-				} else {
-					System.out.println("Error: type of feed not supported");
-					continue;
-				}
+            // parentesis para extraer el nombre del sitio de la url
+            // y si aun no se agrego a la lista de nombres de sitios, se agrega
+            String siteName = extractSiteName(url);
+            if (!siteNames.contains(siteName)) {
+                siteNames.add(siteName);
+            }
 
-				for (int j = 0; j < single.getUlrParamsSize(); j++) {
-					String url = rawUrl.replace("%s", single.getUlrParams(j));
-					String data = requester.getFeed(url, type);
+            GeneralParser<List<Article>> feedParser = null;
 
-					List<Article> articleList = feedParser.parse(data);
+            // se construye el parser correspondiente
+            if (urlType.equals("rss")) {
+                feedParser = new RssParser();
+            } else if (urlType.equals("reddit")) {
+                feedParser = new RedditParser();
+            } else {
+                System.out.println("Error: type of feed not supported");
+                continue;
+            }
 
-					/* llamada al constructor de Feed */
-					Feed feed = new Feed(url);
-					feed.setArticleList(articleList);
+            // se itera sobre cada uno de los parametros de la url
+            // por cada parametro se obtiene el feed, es decir, la lista de articulos
+            // la lista de articulos se agrega a la lista global
+            for (int j = 0; j < single.getUlrParamsSize(); j++) {
+                String urlParam = single.getUlrParams(j);
+                String urlParamFeed = String.format(url, urlParam);
+                String data = requester.getFeed(urlParamFeed, urlType);
 
-					/*
-					 * Llamar a la heuristica para que compute las entidades nombradas de cada
-					 * articulos del feed
-					 */
-					for (int k = 0; k < articleList.size(); k++) {
-						try {
-							/* Imprime las entidades nombradas */
-							System.out
-									.println(
-											"**********************************************************************************************");
-							System.out.println("Article: " + articleList.get(k).getTitle());
-							articleList.get(k).computeNamedEntities(heuristic);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-					continue;
-				}
-			}
-		}
-	}
+                // se parsea la informacion obtenida, se obtiene una lista de articulos
+                // inmediateamente se agrega a la lista global
+                if (globalArticleList == null) {
+                    globalArticleList = feedParser.parse(data);
+                } else {
+                    globalArticleList.addAll(feedParser.parse(data));
+                }
 
+            }
+        }
+
+        // una vez llegado a este punto, la lista global contiene todos los articulos de
+        // todas las suscripciones
+        if (globalArticleList.isEmpty()) {
+            System.out.println("Error: no articles found");
+            return;
+        }
+
+        // dependiendo de si se pasa el parametro -ne o no, se genera el feed o se
+        // computan las entidades nombradas
+        if (args.length == 0) {
+
+            // genero un string con los nombres de los sitios separados por comas
+            String siteNamesString = "";
+            for (int i = 0; i < siteNames.size(); i++) {
+                siteNamesString += siteNames.get(i);
+                if (i != siteNames.size() - 1) {
+                    siteNamesString += ",";
+                }
+            }
+
+            // se genera el feed
+            Feed feed = new Feed(siteNamesString);
+            feed.setArticleList(globalArticleList);
+            // se imprime los sitios de los cuales se obtuvieron los feeds
+            System.out.println("Feed from: " + siteNamesString);
+            // se imprime el feed
+            feed.prettyPrint();
+        } else {
+            // empieza el codigo para computar las entidades nombradas
+            ;
+        }
+
+    }
 }
